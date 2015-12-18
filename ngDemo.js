307d2b3e215c5de3268daef7b7c0ef9f21260112
samiberry <berrysa@mullenmail.com>
2015-12-17 22:25:57 -0700

    Update ngDemo.js
    ADDED ALL OF THE ARRAYS

--- ngDemo.js
+++ ngDemo.js
@@ -56,20 +56,74 @@ ngDemo.controller('mainController', function($scope)
 
 ngDemo.controller('tanktopsController', function($scope)
     {
-    $scope.characters =
+    $scope.tanktopclothes =
         [
             {
-     
-            }
+                articleOfClothing: "Mystery Tanktop",
+                price: "$10.00",
+                image: "Images/mysterycroptop.jpg"
+            },
+                    {
+                articleOfClothing: "Smiley Face Tank Top",
+                price: "$15.00",
+                image: "Images/smiletank.jpg"
+            },
+                    {
+                articleOfClothing: "Vans Tank Top",
+                price: "$12.25",
+                image: "Images/vanstank.jpg"
+            },
+                    {
+                articleOfClothing: "Hang Loose Tank Top",
+                price: "17.50",
+                image: "Images/handtank.jpg"
+            },
+                    {
+                articleOfClothing: "White Crop Top",
+                price: "$9.99",
+                image: "Images/croptopwhite.jpg"
+            },
+                    {
+                articleOfClothing: "Black Tank Top",
+                price: "$10.00",
+                image: "Images/blacktank.jpg"
+            },
         ];
    });
 ngDemo.controller('accessoriesController', function($scope)
     {
-    $scope.characters =
+    $scope.accessories =
         [
             {
-     
-            }
+                articleOfClothing: "Alex and Ani Bangle",
+                price: "$10.00",
+                image: "Images/alexandani.jpg"
+            },
+                    {
+                articleOfClothing: "Aqua Stud Earrings",
+                price: "$15.00",
+                image: "Images/earrings.jpg"
+            },
+                    {
+                articleOfClothing: "Sunglasses",
+                price: "$12.25",
+                image: "Images/glasses.jpg"
+            },
+                    {
+                articleOfClothing: "add me",
+                price: "$20.00",
+                image: "Images/fallsweater.jpg"
+            },
+                    {
+                articleOfClothing: "Original Lokai Bracelets",
+                price: "$18.00",
+                image: "Images/lokai.jpg"
+            },
+                    {
+                articleOfClothing: "Navy Blue Scarf",
+                price: "$10.00",
+                image: "Images/scarf.jpg"
+            },
         ];
    });
 ngDemo.controller('sweaterController', function($scope)
@@ -111,20 +165,74 @@ ngDemo.controller('sweaterController', function($scope)
 ngDemo.
 ngDemo.controller('pantController', function($scope)
     {
-    $scope.characters =
+    $scope.pantclothes =
         [
             {
-            
-            }
+                articleOfClothing: "American Flag Shorts",
+                price: "$10.00",
+                image: "Images/americanflagshorts.jpg"
+            },
+                    {
+                articleOfClothing: "Boyfriend Ripped Jeans",
+                price: "$15.00",
+                image: "Images/boyfriendrippedjeans.jpg"
+            },
+                    {
+                articleOfClothing: "Joggers",
+                price: "$12.25",
+                image: "Images/joggers.jpg"
+            },
+                    {
+                articleOfClothing: "Skinny Jeans",
+                price: "$20.00",
+                image: "Images/jeans.jpg"
+            },
+                    {
+                articleOfClothing: "High Waisted Shorts",
+                price: "$10.00",
+                image: "Images/highwaistedshorts.jpg"
+            },
+                    {
+                articleOfClothing: "Mystery Leggings",
+                price: "$25.00",
+                image: "Images/mysteryleggings.jpg"
+            },
         ];
    });
 ngDemo.controller('shoeController', function($scope)
     {
-    $scope.characters =
+    $scope.shoes =
         [
             {
-            
-            }
+                articleOfClothing: "Uggs With Bows",
+                price: "$170.00",
+                image: "Images/uggsbows.jpg"
+            },
+                    {
+                articleOfClothing: "Construction Boots",
+                price: "$150.00",
+                image: "Images/timbs.jpg"
+            },
+                    {
+                articleOfClothing: "White Converse",
+                price: "$60.00",
+                image: "Images/convers.jpg"
+            },
+                    {
+                articleOfClothing: "Steve Madden Combat Boots",
+                price: "$180.00",
+                image: "Images/combatboots.jpg"
+            },
+                    {
+                articleOfClothing: "Mickey Flower Sweatshirt",
+                price: "$10.00",
+                image: "Images/mineysweater.jpg"
+            },
+                    {
+                articleOfClothing: "Birkenstocks",
+                price: "$160.00",
+                image: "Images/birks.jpg"
+            },
         ];
    });
 
